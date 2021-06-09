2ee563eade175c4aa82826e8c408f01914458ecd
ASHRAY <jha.ashray12@gmail.com>
2021-06-09 12:19:22 +0530

    Added new view components

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello,Ashray !")
-            .padding()
+        return ZStack(content: {
+    RoundedRectangle(cornerRadius: 20)
+    .stroke(lineWidth:3)
+    .padding(.horizontal)
+//    .foregroundColor(.red)
+            
+    Text("Welcome to ios project")
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        .padding()
+                    
+        })
+        .foregroundColor(.red)
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ContentView()
-            ContentView()
-        }
-    }
-}
+
+
+
+
+
+//
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Group {
+//            ContentView()
+//        }
+//    }
+//}
