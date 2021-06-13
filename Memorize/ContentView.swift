8622b614d6e2637f5073b272fe9c7f1027064cf0
ASHRAY <jha.ashray12@gmail.com>
2021-06-13 21:51:37 +0530

    Added Zstack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,33 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-    RoundedRectangle(cornerRadius: 20)
-    .stroke(lineWidth:3)
-    .padding(.horizontal)
-//    .foregroundColor(.red)
+        HStack{
+            ZStack {
+        RoundedRectangle(cornerRadius: 20)
+        .stroke(lineWidth:3)
+            Text("Hello ,World!")
+       }
+            ZStack {
+        RoundedRectangle(cornerRadius: 20)
+        .stroke(lineWidth:3)
+            Text("Hello ,World!")
+       }
+            ZStack
+       {
+        RoundedRectangle(cornerRadius: 20)
+        .stroke(lineWidth:3)
+            Text("Hello ,World!")
+       }
+            ZStack
+       {
+        RoundedRectangle(cornerRadius: 20)
+        .stroke(lineWidth:3)
+            Text("Hello ,World!")
+       }
             
-    Text("Welcome to ios project")
-        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-        .padding()
-                    
-        })
-        .foregroundColor(.red)
-        
+            
+            
+        }
+
+    .padding(.horizontal)
+    .foregroundColor(.red)
+    }}
+   
+   
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
     }
 }
-
-
-
-
-
-
-//
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Group {
-//            ContentView()
-//        }
-//    }
-//}
